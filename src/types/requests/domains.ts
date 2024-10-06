bff28a37ac2eb5ca314bd21d511d0e0a6db4be2a
Agustin Kassis <webmaster@masize.com>
2024-10-06 16:15:40 -0300

    update zod domains schema

--- src/types/requests/domains.ts
+++ src/types/requests/domains.ts
@@ -2,14 +2,25 @@ import { z } from "zod";
 
 export const DomainRegisterSchema = z.object({
   relays: z.array(z.string().url()),
-  adminPubkey: z.string(),
-  rootPrivkey: z.string().optional(),
+  adminPubkey: z
+    .string()
+    .regex(/^[a-f0-9]{64}$/i, "Admin public key must be a 32 bytes hex string"),
+  rootPrivkey: z
+    .string()
+    .regex(/^[a-f0-9]{64}$/i, "Root private key must be a 32 bytes hex string")
+    .optional(),
 });
 
 export const DomainUpdateSchema = z.object({
   relays: z.array(z.string().url()).optional(),
-  adminPubkey: z.string().optional(),
-  rootPrivkey: z.string().optional(),
+  adminPubkey: z
+    .string()
+    .regex(/^[a-f0-9]{64}$/i, "Admin public key must be a 32 bytes hex string")
+    .optional(),
+  rootPrivkey: z
+    .string()
+    .regex(/^[a-f0-9]{64}$/i, "Root private key must be a 32 bytes hex string")
+    .optional(),
 });
 
 export type DomainRegisterBody = z.infer<typeof DomainRegisterSchema>;
