063fa9f26048dfbf8205d09f22f9b0ab6509ebc0
Agustin Kassis <webmaster@masize.com>
2024-10-06 15:46:50 -0300

    fix type

--- src/services/domains.ts
+++ src/services/domains.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import type { PrismaClient } from "@prisma/client";
 import crypto from "crypto";
 import debug from "debug";
 import { isValidDomain, isValidKey } from "@/lib/utils";
