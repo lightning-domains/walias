c5ed521473a8abab4e1a4c32016acb961068eeab
Agustin Kassis <webmaster@masize.com>
2024-10-06 22:45:44 -0300

    add invalid domain to get

--- src/app/api/domains/[domain]/(domains)/get.ts
+++ src/app/api/domains/[domain]/(domains)/get.ts
@@ -1,5 +1,9 @@
+import { isValidDomain } from "@/lib/utils";
 import { DomainsService } from "@/services/domains";
+import debug from "debug";
 import { NextRequest, NextResponse } from "next/server";
+
+const log = debug("app:api:domains:get");
 
 // GET: Get Domain
 export async function GET(
@@ -11,7 +15,15 @@ export async function GET(
     const domainsService = new DomainsService();
     const domainInfo = await domainsService.findDomainById(domain);
 
+    if (!isValidDomain(domain)) {
+      log("Invalid domain name: %s", domain);
+      return NextResponse.json(
+        { reason: "Invalid domain name" },
+        { status: 400 }
+      );
+    }
     if (!domainInfo) {
+      log("Domain %s not found", domain);
       return NextResponse.json(
         { success: false, reason: "Domain not found" },
         { status: 404 }
