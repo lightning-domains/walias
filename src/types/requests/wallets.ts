c2a1ab9f01491adffccc7ff77218e52d3255e4a4
Agustin Kassis <webmaster@masize.com>
2024-10-09 18:38:15 -0300

    add wallet config schema

--- src/types/requests/wallets.ts
+++ src/types/requests/wallets.ts
@@ -16,5 +16,16 @@ export const WalletUpdateSchema = z.object({
   priority: z.number().optional(),
 });
 
+export const WalletConfigSchema = z.object({
+  id: z.string().optional(),
+  lastEventId: z.string().optional(),
+  config: z.record(z.unknown()),
+  provider: z.string(),
+  pubkey: z.string(),
+  waliasId: z.string(),
+  priority: z.number().optional(),
+});
+
+export type WalletConfig = z.infer<typeof WalletConfigSchema>;
 export type WalletCreateBody = z.infer<typeof WalletCreateSchema>;
 export type WalletUpdateBody = z.infer<typeof WalletUpdateSchema>;
